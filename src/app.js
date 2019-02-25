adc24cb2bfab9123597161ef1bda3e7052830059
Kevin Deisz <kevin.deisz@gmail.com>
2019-02-25 11:07:16 -0500

    Initial commit adding frontend infra

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <StrictMode>
+    <h1>QueryPage</h1>
+  </StrictMode>
+);
+
+ReactDOM.render(<App />, document.getElementById("main"));
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, "lib/query_page/static"),
+    filename: "index.js"
+  },
+  entry: path.join(__dirname, "src", "app.js"),
+  module: {
+    rules: [
+      { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ },
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "lib/query_page/static")
+  }
+};
