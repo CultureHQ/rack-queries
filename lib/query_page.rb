ae64dca4f58e51517ee355a1517c0e2d25fd5413
Kevin Deisz <kevin.deisz@gmail.com>
2019-02-25 10:55:50 -0500

    Get initial app working

--- lib/query_page.rb
+++ lib/query_page.rb
@@ -1,5 +1,32 @@
+# frozen_string_literal: true
+
+require 'rack'
 require 'query_page/version'
 
 module QueryPage
-  
+  module App
+    class NotFound
+      def call(env)
+        [404, { 'Content-Type' => 'text/plain' }, ['Not Found']]
+      end
+    end
+
+    STATIC = -File.expand_path(File.join('query_page', 'static'), __dir__)
+
+    class << self
+      def call(env)
+        app.call(env)
+      end
+
+      private
+
+      def app
+        @app ||=
+          Rack::Builder.new do
+            use Rack::Static, urls: %w[/js], root: STATIC, index: 'index.html'
+            run NotFound.new
+          end
+      end
+    end
+  end
 end
