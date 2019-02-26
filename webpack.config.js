53645c8d05d293a0fa22296991526edada4c6841
Kevin Deisz <kevin.deisz@gmail.com>
2019-02-26 13:50:19 -0500

    Start adding eslint infra

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: path.join(__dirname, "src", "app.js"),
   module: {
     rules: [
-      { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ },
+      { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ }
     ]
   }
 };
